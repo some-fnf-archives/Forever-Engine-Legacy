1ffea40225a07639cc99164ce45f2ec824a88def
Gabriela Bestial <93128334+BeastlyGabi@users.noreply.github.com>
2023-01-27 19:48:12 -0400

    remove references of "Legacy" on setup.bat
    as this ain't a fork

--- setup-files/setup.bat
+++ setup-files/setup.bat
@@ -1,13 +1,13 @@
 @echo off
-title Forever Engine Legacy Setup - Start
-echo Welcome to the Forever Engine Legacy Automatic Setup!
+title Forever Engine Setup - Start
+echo Welcome to the Forever Engine Automatic Setup!
 TIMEOUT 3 >nul
 echo This will automatically install all of the needed libraries and dependencies for compiling.
 TIMEOUT 2 >nul
 pause
 cls
 
-title Forever Engine Legacy Setup - Installing Haxe
+title Forever Engine Setup - Installing Haxe
 echo Time to install Haxe - the open-source toolkit.
 TIMEOUT 2 >nul
 echo Make sure you download the latest version!
@@ -20,7 +20,7 @@ echo Press any key to continue once you have finished installing Haxe.
 pause >nul
 cls
 
-title Forever Engine Legacy Setup - Installing HaxeFlixel
+title Forever Engine Setup - Installing HaxeFlixel
 echo It is time to install the engine that Funkin' uses - HaxeFlixel.
 TIMEOUT 2 >nul
 echo Installing Haxeflixel...
@@ -36,7 +36,7 @@ echo Press any key to continue once you have finished installing HaxeFlixel.
 pause >nul
 cls
 
-title Forever Engine Legacy Setup - Install Git
+title Forever Engine Setup - Install Git
 echo Now, we have to install Git - software for distributed version control.
 TIMEOUT 2 >nul
 echo Make sure you download the latest version!
@@ -49,7 +49,7 @@ echo Press any key to contiue once you have finished installing Git.
 pause >nul
 cls
 
-title Forever Engine Legacy Setup - Installing Additional Libraries
+title Forever Engine Setup - Installing Additional Libraries
 echo After installing Haxe, HaxeFlixel and Git, it is time to install the additional libraries needed for compiling.
 TIMEOUT 2 >nul
 echo Press any key to install polymod.
@@ -66,7 +66,7 @@ echo Press any key to continue with the installation of VSCommunity and its depe
 pause >nul
 cls
 
-title Forever Engine Legacy Setup - Installing Windows Dependencies
+title Forever Engine Setup - Installing Windows Dependencies
 echo Moving on from apps and libraries, it is time to install VSCommunity and the dependencies needed for compiling.
 TIMEOUT 2 >nul
 set /p menu="Would you like to install Visual Studio Community and components? [Y/N]"
@@ -77,14 +77,14 @@ set /p menu="Would you like to install Visual Studio Community and components? [
        cls
 
 :ProceedWithVSCommunityInstall
-title Forever Engine Legacy Setup - Installing Windows Dependencies
+title Forever Engine Setup - Installing Windows Dependencies
 set /p menu2="For which version of Windows would you like to install Visual Studio Community and components? [10/11]"
         if %menu2%==10 goto InstallVSCommunityWin10
         if %menu2%==11 goto InstallVSCommunityWin11
         cls
 
 :InstallVSCommunityWin10
-title Forever Engine Legacy - Installing Visual Studio Community and Components (Windows 10)
+title Forever Engine - Installing Visual Studio Community and Components (Windows 10)
 curl -# -O https://download.visualstudio.microsoft.com/download/pr/5c9aef4f-a79b-4b72-b379-14273860b285/58398a76f32a0149d38fba79bbf71b6084ccd4200ea665bf2bcd954cdc498c7f/vs_Community.exe
 vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.20348
 del vs_Community.exe
@@ -93,7 +93,7 @@ pause >nul
 goto SkipVSCommunity
 
 :InstallVSCommunityWin11
-title Forever Engine Legacy - Instaling Visual Studio Community and Components (Windows 11)
+title Forever Engine - Instaling Visual Studio Community and Components (Windows 11)
 curl -# -O https://download.visualstudio.microsoft.com/download/pr/5c9aef4f-a79b-4b72-b379-14273860b285/58398a76f32a0149d38fba79bbf71b6084ccd4200ea665bf2bcd954cdc498c7f/vs_Community.exe
 vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows11SDK.22621
 del vs_Community.exe
@@ -103,7 +103,7 @@ goto SkipVSCommunity
 
 :SkipVSCommunity
 cls
-title Forever Engine Legacy Setup - Success
+title Forever Engine Setup - Success
 echo Setup successful. Press any key to exit.
 pause >nul
 exit
